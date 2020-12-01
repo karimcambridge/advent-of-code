0cd631ce95af6f066c382aae2d8d76321c4f58f4
Karim Cambridge <karimcambridge@gmail.com>
2020-12-01 17:04:42 -0400

    update: 2020 day1

---
+++ 2020/day1/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf8').split('\n').map(x => parseInt(x));
+
+// part 1
+
+for(let i = 0; i < input?.length; ++i) {
+	const
+		target = input.find(x => input[i] + x === 2020)
+	;
+	if(target) {
+		console.log(target, input[i]);
+		console.log(`solution 1: `, target * input[i]);
+	}
+};
+
+// part 2
+
+for(let i = 0; i < input?.length; ++i) {
+	for(let j = 0; j < input?.length; ++j) {
+		const
+			target = input.find(x => input[i] + input[j] + x === 2020)
+		;
+		if(target) {
+			console.log(target, input[i], input[j]);
+			console.log(`solution 2: `, target * input[i] * input[j]);
+			return;
+		}
+	};
+};
