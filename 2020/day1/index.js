238fc039db7a66a7bc81ded8462a1c99b03afc0a
Karim Cambridge <karimcambridge@gmail.com>
2020-12-01 17:07:02 -0400

    update: Fix day 1 code

--- 2020/day1/index.js
+++ 2020/day1/index.js
@@ -3,28 +3,35 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8').split('\n').map(x => parseInt(x));
 
 // part 1
-
-for(let i = 0; i < input?.length; ++i) {
-	const
-		target = input.find(x => input[i] + x === 2020)
-	;
-	if(target) {
-		console.log(target, input[i]);
-		console.log(`solution 1: `, target * input[i]);
-	}
-};
-
-// part 2
-
-for(let i = 0; i < input?.length; ++i) {
-	for(let j = 0; j < input?.length; ++j) {
+export const problem1 = () => {
+	for(let i = 0; i < input?.length; ++i) {
 		const
-			target = input.find(x => input[i] + input[j] + x === 2020)
+			target = input.find(x => input[i] + x === 2020)
 		;
 		if(target) {
-			console.log(target, input[i], input[j]);
-			console.log(`solution 2: `, target * input[i] * input[j]);
-			return;
+			console.log(target, input[i]);
+			console.log(`solution 1: `, target * input[i]);
 		}
 	};
-};
+	return;
+}
+
+// part 2
+export const problem2 = () => {
+	for(let i = 0; i < input?.length; ++i) {
+		for(let j = 0; j < input?.length; ++j) {
+			const
+				target = input.find(x => input[i] + input[j] + x === 2020)
+			;
+			if(target) {
+				console.log(target, input[i], input[j]);
+				console.log(`solution 2: `, target * input[i] * input[j]);
+				return;
+			}
+		};
+	};
+	return;
+}
+
+problem1();
+problem2();
