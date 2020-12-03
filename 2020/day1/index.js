3a58a811758d497fa393355871304baea10785ab
Karim <4283893+karimcambridge@users.noreply.github.com>
2020-12-03 00:58:56 -0400

    day1: revamp in a fun way

--- 2020/day1/index.js
+++ 2020/day1/index.js
@@ -1,37 +1,36 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('input.txt', 'utf8').split('\n').map(x => parseInt(x));
+const
+	input = fs.readFileSync('input.txt', 'utf8').split('\n').map(x => parseInt(x))
+;
 
 // part 1
-export const problem1 = () => {
-	for(let i = 0; i < input?.length; ++i) {
-		const
-			target = input.find(x => input[i] + x === 2020)
-		;
-		if(target) {
-			console.log(target, input[i]);
-			console.log(`solution 1: `, target * input[i]);
-		}
-	};
-	return;
-}
+const puzzle1 = () => input.every(a => {
+	const
+		target = input.find(b => a + b === 2020)
+	;
+	if(target) {
+		console.log(target, a);
+		console.log(`puzzle solution 1: `, target * a);
+		return false;
+	}
+	return true;
+});
 
 // part 2
-export const problem2 = () => {
-	for(let i = 0; i < input?.length; ++i) {
-		for(let j = 0; j < input?.length; ++j) {
-			const
-				target = input.find(x => input[i] + input[j] + x === 2020)
-			;
-			if(target) {
-				console.log(target, input[i], input[j]);
-				console.log(`solution 2: `, target * input[i] * input[j]);
-				return;
-			}
-		};
-	};
-	return;
-}
+const puzzle2 = () => input.every(a => {
+	return input.every(b => {
+		const
+			target = input.find(c => a + b + c === 2020)
+		;
+		if(target) {
+			console.log(target, a, b);
+			console.log(`puzzle solution 2: `, target * a * b);
+			return false;
+		}
+		return true;
+	});
+});
 
-problem1();
-problem2();
+puzzle1();
+puzzle2();
